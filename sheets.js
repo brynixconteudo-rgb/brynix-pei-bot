742ca34d80979ba73cd57086c92fafa3f2b2d8d2
brynixconteudo-rgb <brynixconteudo@gmail.com>
2025-10-04 12:38:04 -0300

    Update sheets.js

--- sheets.js
+++ sheets.js
@@ -1,39 +1,17 @@
-// sheets.js
-
-const { google } = require("googleapis");
-
-const SHEET_ID = process.env.SHEETS_SPREADSHEET_ID;
-const CLIENT_ID = process.env.GOOGLE_OAUTH_CLIENT_ID;
-const CLIENT_SECRET = process.env.GOOGLE_OAUTH_CLIENT_SECRET;
-const REFRESH_TOKEN = process.env.GOOGLE_OAUTH_REFRESH_TOKEN;
-
-let sheetsAPI;
-
-function getSheetsClient() {
-  if (!sheetsAPI) {
-    const oauth2Client = new google.auth.OAuth2(
-      CLIENT_ID,
-      CLIENT_SECRET
-    );
-
-    oauth2Client.setCredentials({
-      refresh_token: REFRESH_TOKEN,
-    });
-
-    sheetsAPI = google.sheets({
-      version: "v4",
-      auth: oauth2Client,
-    });
-  }
-
-  return sheetsAPI;
-}
-
 async function appendLeadRow(rowValues, sheetName = "Leads") {
   const sheets = getSheetsClient();
   const range = `${sheetName}!A1`;
 
   try {
+    // 🔍 Adiciona log com as abas visíveis
+    const metadata = await sheets.spreadsheets.get({
+      spreadsheetId: SHEET_ID,
+    });
+
+    const sheetNames = metadata.data.sheets.map(s => s.properties.title);
+    console.log("📋 Abas visíveis na planilha:", sheetNames);
+
+    // Tenta inserir normalmente
     await sheets.spreadsheets.values.append({
       spreadsheetId: SHEET_ID,
       range,
@@ -50,7 +28,3 @@ async function appendLeadRow(rowValues, sheetName = "Leads") {
     throw err;
   }
 }
-
-module.exports = {
-  appendLeadRow,
-};
